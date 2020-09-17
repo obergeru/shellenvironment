62daa7216c072698f08566cf2a4b7ad47595adb9
Øyvind Bergerud <oyvind.bergerud@gjensidige.com>
2020-09-17 22:49:58 +0200

    update

--- setup.sh
+++ setup.sh
@@ -33,3 +33,12 @@ cp .tmux.conf ~/
 cp .vimtmux.conf ~/
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
