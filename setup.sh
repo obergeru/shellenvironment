259fa84cd95f1dd6edc89d2a4fc32d5f9d68fa21
Øyvind Bergerud <oyvind.bergerud@gjensidige.com>
2021-02-23 18:20:08 +0100

    autocomplete

--- setup.sh
+++ setup.sh
@@ -49,3 +49,5 @@ mkdir ~/src
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
